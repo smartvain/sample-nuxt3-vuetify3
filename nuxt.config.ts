6e20219ee48ac647302ca2752ae4dd427ac1d825
Smart Vain <zlc.blue15210@gmail.com>
2022-09-25 20:47:56 +0900

    change: directory structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  rootDir: './src',
   css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
   build: {
     transpile: ["vuetify"],
